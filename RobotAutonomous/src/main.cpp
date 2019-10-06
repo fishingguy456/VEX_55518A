606b50170dd167195fa3a00d4bf5049fb30e3df9
fishingguy456 <43834326+fishingguy456@users.noreply.github.com>
2019-10-06 13:09:25 -0400

    added more stuff to get started

--- RobotAutonomous/src/main.cpp
+++ RobotAutonomous/src/main.cpp
@@ -7,22 +7,24 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 #include "vex.h"
-#include "config.h"
+#include "robotConfig.h"
+//#include "autoFunction"
 using namespace vex;
-//dab pls hahahhahahahahha
 // A global instance of vex::brain used for printing to the V5 brain screen
-vex::brain       Brain;
-
 // define your global instances of motors and other devices here
 
 
 int main() {
-    int count = 0;
-    
-    MotorDriveL.spin(directionType::fwd,3,velocityUnits::rpm);
-    while(1) {
-        Brain.Screen.printAt( 10, 50, "Hello V5 %d", count++ );
-        // Allow other tasks to run
-        this_thread::sleep_for(10);
+    vex::task::sleep(2000);
+    Brain.Screen.print("User Program has Started");
+    /*
+    double wheelDiameterCM = 10.16;
+    double travelTargetCM = 20;
+    double circumference = wheelDiameterCM*3.141592;
+    double degreesToRotate = (360*travelTargetCM)/circumference;
+    */
+    while(Controller.ButtonR1.pressing()){
+      MotorIntakeL.spin(directionType::fwd, 50, velocityUnits::rpm);
+      MotorIntakeR.spin(directionType::rev, 50, velocityUnits::rpm);
     }
 }
